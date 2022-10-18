5741f81a4679d6fd7126233edb502d947eeba025
Pachuli <113481647+Pachuli666@users.noreply.github.com>
2022-10-18 18:57:39 -0500

    2P_E02_Eventos
    Script para evaluar el score y activar un evento.

---
+++ 2P_Arturo/Assets/Scripts/PracticaEventos.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PracticaEventos : MonoBehaviour
+{
+
+    public TMP_Text Notification;
+    public int Counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   public void PlusPoint()
+    {
+        Counter++;
+        
+    }
+
+    public void CheckPoints()
+    {
+
+    }
+      
+}
